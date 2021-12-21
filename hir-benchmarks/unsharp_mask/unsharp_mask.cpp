b96e754080bc612ac9a898f907d26a580dcc7ae2
kingshuk1000 <kingshuk1000@gmail.com>
2021-12-21 16:54:28 +0530

    CMake based compilation

--- hir-benchmarks/unsharp_mask/unsharp_mask.cpp
+++ hir-benchmarks/unsharp_mask/unsharp_mask.cpp
@@ -4,9 +4,19 @@
 using namespace cv;
 using namespace std;
 
+//helper functions.
+
+Mat convX(Mat& image, Mat& kernel){
+	Mat output;
+	for(int i=0;i<image)
+	return output;
+}
+
+
 // Driver code
 int main(int argc, char** argv)
 {
+	float blur_kernel_values[5] = {1,1,1,1,1};
 	// Read the image file as
 	// imread("default.jpg");
 	Mat image = imread("lenna.png",
@@ -22,12 +32,18 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	
 	// Show Image inside a window with
 	// the name provided
 	imshow("Window Name", image);
 
+	Mat kernel = Mat(1,5,CV_32F,blur_kernel_values);
+	Mat blurx = convX(image,kernel);
+
 	// Wait for any keystroke
 	waitKey(0);
+
+
 	return 0;
 }
 
