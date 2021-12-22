4d93541b8d4b9e9487ce4e6e8fa2a4e0594a058e
kingshuk1000 <kingshuk1000@gmail.com>
2021-12-22 22:10:10 +0530

    Misc

--- hir-benchmarks/unsharp_mask/unsharp_mask.cpp
+++ hir-benchmarks/unsharp_mask/unsharp_mask.cpp
@@ -6,21 +6,37 @@ using namespace std;
 
 //helper functions.
 
-Mat convX(Mat& image, Mat& kernel){
-	Mat output;
-	for(int i=0;i<image)
-	return output;
+void convX(Mat& output, Mat& image, Mat& kernel){
+	Mat img;
+
+	image.convertTo(img,CV_32F);
+
+	for(int i=0;i<img.rows-kernel.rows;i++){
+		for(int j=0;j<img.cols-kernel.cols;j++){
+			output.at<float>(i,j) =0;
+			for(int ii=0;ii<kernel.rows;ii++){
+				for(int jj=0;jj<kernel.rows;jj++){
+					output.at<float>(i,j)+=kernel.at<float>(ii,jj)*img.at<float>(i+ii,j+jj);
+				}
+			}
+		}
+	}
 }
 
+void showFloatMat(Mat& img){
+	Mat img2;
+	img.convertTo(img2,CV_8U);
+	imshow("Window Name", img2);
+}
 
 // Driver code
 int main(int argc, char** argv)
 {
-	float blur_kernel_values[5] = {1,1,1,1,1};
+	float blur_kernel_values[5] = {1/16.0, 4/16.0, 6/16.0, 4/16.0, 1/16.0};
 	// Read the image file as
 	// imread("default.jpg");
-	Mat image = imread("lenna.png",
-					IMREAD_GRAYSCALE);
+	Mat image = imread("../lenna.png",
+			IMREAD_GRAYSCALE);
 
 	// Error Handling
 	if (image.empty()) {
@@ -32,14 +48,15 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	
+
 	// Show Image inside a window with
 	// the name provided
-	imshow("Window Name", image);
 
-	Mat kernel = Mat(1,5,CV_32F,blur_kernel_values);
-	Mat blurx = convX(image,kernel);
+	Mat kernel = Mat(5,1,CV_32F,blur_kernel_values);
+	Mat blurx (image.rows,image.cols,CV_32F,0.0);
 
+	convX(blurx,image,kernel);
+	showFloatMat(blurx);
 	// Wait for any keystroke
 	waitKey(0);
 
