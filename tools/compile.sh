214e52cca08050342f620b5c68a52a464e922105
kingshukmajumder <kingshuk1000@gmail.com>
2023-10-28 16:40:35 +0530

    cleanup

--- tools/compile.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p build
-../circt/build/bin/circt-opt -instrument-cosim="dir=./build/$1 entry=gesummv_hir" $1.mlir>/dev/null
-../circt/build/bin/mlir-opt --lower-affine --convert-scf-to-cf --finalize-memref-to-llvm --convert-func-to-llvm='use-bare-ptr-memref-call-conv' --reconcile-unrealized-casts ./build/$1/cpu-module.mlir >./build/$1/cpu_llvm.mlir
--- tools/recorder.c
+++
@@ -1,2 +0,0 @@
-#include <stdio.h>
-void record(int i) { printf("%d\n", i); }
